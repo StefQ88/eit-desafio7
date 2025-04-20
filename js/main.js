c2a08599cd4a1b1cd4045f04e3c85100587ee063
Stefani Quiolla <stefiquio@gmail.com>
2025-04-20 19:47:53 -0300

    add files js and files css

---
+++ js/main.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const styleButton = document.querySelector(".button-style"); //obtengo boton para cambiar estilo
+
+    const stylesheetLink = document.querySelector('link[rel="stylesheet"][href*="css/"]'); //obtengo link activo
+
+    // lista de estilos disponibles
+    const styles = [
+        "css/estilos.css",
+        "css/urbano.css",
+        "css/peliculas.css"
+    ]
+
+    let current = 0; // indice que indica estilo actual, comienza en original
+    document.getElementById("first-name").focus()
+
+    styleButton.addEventListener("click", () => {
+        current = (current + 1) % styles.length; // calcula indice del segundo estilo
+
+        stylesheetLink.setAttribute("href", styles[current])   // Cambia el href del <link> al nuevo archivo CSS
+
+    })
+
+})
